40606d08e35d2b387b7bde045bfb7cf1b2e77796
Billy Bastardi <billy@yext.com>
2014-09-12 19:03:44 -0500

    add comments to client config

--- client/config.go
+++ client/config.go
@@ -11,6 +11,8 @@ import (
 	"github.com/imdario/mergo"
 )
 
+// GuppyConfig is used to tell guppy how to handle packages.
+// e.g. What registry to pull from, and what directories to clone them to.
 type GuppyConfig struct {
 	Cwd             string
 	Directory       string
@@ -19,6 +21,10 @@ type GuppyConfig struct {
 	Color           bool
 }
 
+// NewGuppyConfig will return a config based on a few things;
+// It first creates some default settings and merges/overwrites that with the root config (~/.guppy)
+// It then looks for a project specific config (current working directory) and will merge/overwrite
+// that previous result with the project specific config settings.
 func NewGuppyConfig() *GuppyConfig {
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -35,7 +41,7 @@ func NewGuppyConfig() *GuppyConfig {
 
 	user, err := user.Current()
 	if err == nil {
-		err = gc.Load(user.HomeDir)
+		err = gc.load(user.HomeDir)
 
 		if err != nil {
 			glog.Println(fmt.Sprintf("Failed to load guppy root config `%v`", err))
@@ -43,7 +49,7 @@ func NewGuppyConfig() *GuppyConfig {
 	}
 
 	if cwd != user.HomeDir {
-		err = gc.Load(cwd)
+		err = gc.load(cwd)
 		if err != nil {
 			glog.Println(fmt.Sprintf("Failed to load guppy project config `%v`", err))
 		}
@@ -52,7 +58,7 @@ func NewGuppyConfig() *GuppyConfig {
 	return gc
 }
 
-func (gc *GuppyConfig) Load(cfgPath string) error {
+func (gc *GuppyConfig) load(cfgPath string) error {
 	cfg, err := ioutil.ReadFile(path.Join(cfgPath, guppyFile))
 	if err != nil || cfg == nil {
 		return err
--- client/models.go
+++ client/models.go
@@ -10,8 +10,3 @@ type GuppyLog struct{}
 func (gl *GuppyLog) Println(v interface{}) {
 	log.Println(fmt.Sprintf("[guppy] %v", v))
 }
-
-type Guppy struct {
-	projectPath string
-	guppyPath   string
-}
